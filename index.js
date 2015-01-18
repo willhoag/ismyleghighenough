92e713fc9e6cede6fcaf7a7b5755d2e73a25cd44
willhoag <willhoag@willhoag.com>
2015-01-18 11:41:38 -0800

    update styles

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var createObjectURL, div, dragDrop, img;
+var body, createObjectURL, div, dragDrop, img;
 
 dragDrop = require("drag-drop");
 
@@ -7,13 +7,16 @@ img = document.getElementById("img");
 
 div = document.getElementById("text");
 
+body = document.getElementsByTagName("body")[0];
+
 createObjectURL = function(blob) {
   return (window.URL || window.webkitURL).createObjectURL(blob);
 };
 
 dragDrop(".dropZone", function(files) {
   img.src = createObjectURL(files[0]);
-  return div.innerHTML = "<h1 id='quote'>NOT GOOD ENOUGH!!!</h1>";
+  div.innerHTML = "<h1 id='quote'>NOT GOOD ENOUGH!!!</h1>";
+  return body.setAttribute('class', 'done');
 });
 
 
